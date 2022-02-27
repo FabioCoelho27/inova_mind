95b47aee0d0f4ecb99981a8d7203dfa4e166ab0a
FabioCoelho27 <fabioolivc@gmail.com>
2022-02-27 20:43:32 -0300

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,8 @@ Sidekiq::Web.use ActionDispatch::Session::CookieStore, key: "_interslice_session
 Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq' 
   resources :reports, only: [:quote]
-  get 'quotes/:tag', to:'quotes#quotes'
-  get 'quotes', to: 'quotes#index'
-  post 'quotes/clean', to: 'quotes#clean_all'
-  post 'quotes/:tag/clean', to: 'quotes#clean'
-  post 'quotes/:tag/reset', to: 'quotes#reset'
+  get 'quotes/:tag',        to:'quotes#quotes'
+  get 'quotes',             to: 'quotes#index'
   post 'user/signup'
   post 'user/login'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
